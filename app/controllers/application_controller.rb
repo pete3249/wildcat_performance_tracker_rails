87bcb4a068583e85c744c24a999a0f383bbfab53
pete3249 <katelynrpeterson16@gmail.com>
2020-11-04 14:25:36 -0500

    Add Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    # like a hook
+    # allows us to add code that will run before all controller actions in this controller
+    # if in application controller, going to run before every action
+    # check if we are in controller where devise is a helper
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
