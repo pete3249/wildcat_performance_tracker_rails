33bfc3db8588c81a52c367e01263a9037b3dfe5c
pete3249 <katelynrpeterson16@gmail.com>
2020-11-04 15:42:54 -0500

    Add macros and validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :performance_tests
+  has_many :students, through: :performance_tests
+  has_many :test_types, through: :performance_tests
+  validates :name, presence: true
 end
