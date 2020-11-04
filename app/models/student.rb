f4e783b83de0f6a420268181a5fb5326d017757c
pete3249 <katelynrpeterson16@gmail.com>
2020-11-04 15:41:20 -0500

    Add macros and validations for student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ApplicationRecord
+    has_many :performance_tests
+    has_many :users, through: :performance_tests
+    has_many :test_types, through: :performance_tests
+
+    validates :first_name, :last_name, :grad_year, presence: true
+
 end
