3ef070129395824bc83e06a18b05303319e42ca0
pete3249 <katelynrpeterson16@gmail.com>
2020-11-04 15:15:31 -0500

    Add students

---
+++ app/helpers/students_helper.rb
@@ -0,0 +1,7 @@
+module StudentsHelper
+
+    def full_name(student)
+        student.first_name + " #{student.last_name}"
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_184852) do
+ActiveRecord::Schema.define(version: 2020_11_04_193757) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "grad_year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
