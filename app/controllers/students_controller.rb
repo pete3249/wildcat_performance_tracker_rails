a4eb14bfdc3eca804b467e6a199f2770e6da9802
pete3249 <katelynrpeterson16@gmail.com>
2020-11-05 15:08:02 -0500

    Add restful routes for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,37 @@
 class StudentsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_student, only: [:show, :edit, :update, :destroy]
 
     def index
         @students = Student.all
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def set_student
+        @student = Student.find(params[:id])
     end
 
 end
