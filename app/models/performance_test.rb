0e55d3136f8b99fbd051528989491072189126c1
pete3249 <katelynrpeterson16@gmail.com>
2020-11-05 10:16:21 -0500

    Uniqueness for student, date_taken, user

--- app/models/performance_test.rb
+++ app/models/performance_test.rb
@@ -4,4 +4,6 @@ class PerformanceTest < ApplicationRecord
   belongs_to :test_type
 
   validates :result, :date_taken, presence: true
+  validates :result, uniqueness: { scope: [:date_taken, :test_type, :user_id], message: "Performance test has already been recorded for this student."}
+  
 end
