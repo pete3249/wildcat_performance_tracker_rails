b7014aac13da22305f99bf243bf906fb7217a1c1
pete3249 <katelynrpeterson16@gmail.com>
2020-11-05 15:07:45 -0500

    Add rescue from RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,17 @@ class ApplicationController < ActionController::Base
     # if in application controller, going to run before every action
     # check if we are in controller where devise is a helper
     before_action :configure_permitted_parameters, if: :devise_controller?
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
+
+
+    def not_found
+        flash[:error] = "Not found"
+        redirect_to students_path
+    end 
+  
 end
+
