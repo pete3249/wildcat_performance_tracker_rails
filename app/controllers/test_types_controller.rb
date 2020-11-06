08590ab54ed34d012f934fb8e5dd4923ee17943e
pete3249 <katelynrpeterson16@gmail.com>
2020-11-06 08:19:21 -0500

    Add restful routes

--- app/controllers/test_types_controller.rb
+++ app/controllers/test_types_controller.rb
@@ -1,2 +1,47 @@
 class TestTypesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_test_type, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @test_types = TestType.all
+    end
+
+    def show
+    end
+
+    def new
+        @test_type = TestType.new
+    end
+
+    def create
+        @test_type = TestType.new(test_type_params)
+
+        if @test_type.save
+            redirect_to test_type_path(@test_type)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @test_type.update(test_type_params)
+            redirect_to test_type_path(@test_type)
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def test_type_params
+        params.require(:test_type).permit(:name)
+    end 
+
+    def set_test_type
+        @test_type = TestType.find(params[:id])
+    end
+
 end
