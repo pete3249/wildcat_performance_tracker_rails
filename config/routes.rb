208727ec51a069cb479cb4c923d0cee3f2c6d7c0
pete3249 <katelynrpeterson16@gmail.com>
2020-11-10 18:53:08 -0500

    Add nested route for users/:user_id/students

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   resources :performance_tests
   resources :test_types
-  resources :students
-  devise_for :users
+
+  resources :students do
+    resources :performance_tests, only: [:index, :new, :show]
+  end 
+
+  devise_for :users, :path => 'accounts'
+
+  resources :users do
+      resources :students
+  end
 
   root 'students#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
