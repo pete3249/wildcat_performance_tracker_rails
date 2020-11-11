06b68d4d771dec7b61aa13472ede70245c791ac6
pete3249 <katelynrpeterson16@gmail.com>
2020-11-11 11:04:16 -0500

    Add potential helper

--- app/helpers/performance_tests_helper.rb
+++ app/helpers/performance_tests_helper.rb
@@ -1,7 +1,6 @@
 module PerformanceTestsHelper
 
-    def test_name(performance_test)
-        performance_test.test_type.name
-    end 
-
+    # def test_name(performance_test)
+    #     performance_test.test_type.name
+    # end 
 end
