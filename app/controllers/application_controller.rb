d3ece9b5edba04cfab584032c285ff9cecc90562
pete3249 <katelynrpeterson16@gmail.com>
2020-11-12 09:21:00 -0500

    Add protecct from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
     # like a hook that allows us to add code that will run before all controller actions in this controller
     # if in application controller, going to run before every action
     # check if we are in controller where devise is a helper
+    protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
     end
   
 end
