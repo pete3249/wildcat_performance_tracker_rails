a15509e69dc0c6043bd761d24be951df43e36eed
pete3249 <katelynrpeterson16@gmail.com>
2020-11-11 22:38:46 -0500

    Remove nested users route

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,7 @@ Rails.application.routes.draw do
     resources :performance_tests, only: [:index, :new, :show]
   end 
 
-  devise_for :users, :path => 'accounts'
-
-  resources :users do
-      resources :students
-  end
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'students#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
