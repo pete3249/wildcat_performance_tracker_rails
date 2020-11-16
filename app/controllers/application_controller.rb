2b1195410c2fd6005cd11968fd7371a8160ea88a
pete3249 <katelynrpeterson16@gmail.com>
2020-11-16 10:20:26 -0500

    Remove comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-    # like a hook that allows us to add code that will run before all controller actions in this controller
-    # if in application controller, going to run before every action
-    # check if we are in controller where devise is a helper
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
 
