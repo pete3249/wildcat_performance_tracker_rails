2508e895a7cdd330e7b7f5ab81f79185520b5c89
pete3249 <katelynrpeterson16@gmail.com>
2020-11-16 13:54:54 -0500

    Add nested_attributes method

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,8 @@ class Student < ApplicationRecord
     has_many :users, through: :performance_tests
     has_many :test_types, through: :performance_tests
 
+    accepts_nested_attributes_for :performance_tests
+    
     validates :name, :grad_year, presence: true
     validates :name, uniqueness: { scope: [:grad_year], message: "and graduation year match an existing student"}
 
