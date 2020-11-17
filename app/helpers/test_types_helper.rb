70d284edc02a70a9183585972967d1154133cc6e
pete3249 <katelynrpeterson16@gmail.com>
2020-11-17 00:03:51 -0500

    Remove helpers from TestTypes

--- app/helpers/test_types_helper.rb
+++ app/helpers/test_types_helper.rb
@@ -1,20 +1,3 @@
 module TestTypesHelper
 
-    def add_units(performance_test)
-        if performance_test.test_type_id == "1" || performance_test.test_type_id == "2"
-            labeled_test = add_inches(performance_test.result)
-        else
-            labeled_test = add_secs(performance_test.result)
-        end
-        labeled_test
-    end 
-
-    def add_inches(result)
-        "#{result} in"
-    end 
-
-    def add_secs(result)
-        "#{result} sec"
-    end 
-
 end
